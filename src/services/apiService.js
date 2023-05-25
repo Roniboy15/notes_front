f3f5c61a153a47c4a98f011a70e8ef29ff90c43d
Your Name <you@example.com>
2023-05-25 13:40:31 +0300

    server_url

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'http://localhost:10000/';
+const API_KEY = 'https://notes-taking-app-server.onrender.com/';
 
 // Function to get the token from local storage
 const getToken = () => {
